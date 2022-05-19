569aff3d4d28251c3b57a262fa2b879b6b0e49a5
Waris Oloyede <oloyedewaris@gmail.com>
2022-05-19 21:46:48 +0100

    first

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { View, Button } from 'react-native'
+import { registerYodoAds, showBannerAds, dismissBannerAds, showInterstitialAds, showRewardedAds } from './adFunctions';
+import { NativeModules } from 'react-native';
+const { Yodo1MASAds } = NativeModules;
+
+const App = () => {
+  useEffect(() => {
+    registerYodoAds()
+  }, [])
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Button onPress={() => showBannerAds()} title='Show Banner' />
+      <Button onPress={() => dismissBannerAds()} title='Dismiss Banner' />
+      <Button onPress={() => showInterstitialAds()} title='Show Interstial Ads' />
+      <Button onPress={() => showRewardedAds()} title='Show Rewarded Ads' />
+    </View>
+  );
+}
+
+export default App;
